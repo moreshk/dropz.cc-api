07169300159d9abf838be2df394185f2df01f51c
Pranav <pranavkp.me@outlook.com>
2024-06-17 15:16:34 +0530

    added tokens to the widget sdk Api

--- src/controllers/skd-controllers.ts
+++ src/controllers/skd-controllers.ts
@@ -1,5 +1,6 @@
 import { widgetIdParamsSchema } from '@/schema/widgets';
 import { getBalance } from '@/services/sdk-services';
+import { getAllTokens } from '@/services/token-services';
 import { getWidgetById } from '@/services/widget-services';
 import { createHandler } from '@/utils/create';
 import { BackendError } from '@/utils/errors';
@@ -7,9 +8,10 @@ import { BackendError } from '@/utils/errors';
 export const handleGetSdkWidgetById = createHandler(widgetIdParamsSchema, async (req, res) => {
   const { id } = req.params;
   const widget = await getWidgetById(id);
-  if (!widget)
+  const tokens = await getAllTokens();
+  if (!widget || !tokens)
     throw new BackendError('NOT_FOUND');
-  res.status(200).json({ widget });
+  res.status(200).json({ widget, tokens });
 });
 
 export const handelGetBalance = createHandler(widgetIdParamsSchema, async (req, res) => {
