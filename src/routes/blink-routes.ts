ebc89e574dade1b3ee9f384e4bd34d0a7df4111a
Pranav <pranavkp.me@outlook.com>
2024-06-26 15:35:16 +0530

    fix endpoint

--- src/routes/blink-routes.ts
+++ src/routes/blink-routes.ts
@@ -6,7 +6,7 @@ export default createRouter((router: Router) => {
   router.get('/details', handelDefaultGetBlinkData);
   router.post('/action/buy/default/:amount', handelGetDefaultBuyTransaction);
   // router.post('/action/sell/default/:amount', handelGetDefaultSellTransaction);
-  router.get('/details/:id', handelGetBlinkMetaData);
+  router.get('/details/widget/:id', handelGetBlinkMetaData);
   router.get('/action/buy/:id/:amount', handelGetPaymentTransaction);
   router.get('/action/sell/:id/:amount', handelGetBlinkMetaData);
 });
