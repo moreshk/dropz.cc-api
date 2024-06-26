fc9615891c525cbc243016c4d129ca4b6e58a486
Pranav <pranavkp.me@outlook.com>
2024-06-26 15:44:33 +0530

    fix method

--- src/routes/blink-routes.ts
+++ src/routes/blink-routes.ts
@@ -1,12 +1,10 @@
 import type { Router } from 'express';
 import { createRouter } from '@/utils/create';
-import { handelDefaultGetBlinkData, handelGetBlinkMetaData, handelGetDefaultBuyTransaction, handelGetDefaultSellTransaction, handelGetPaymentTransaction } from '@/controllers/blink-controllers';
+import { handelDefaultGetBlinkData, handelGetBlinkMetaData, handelGetDefaultBuyTransaction, handelGetPaymentTransaction } from '@/controllers/blink-controllers';
 
 export default createRouter((router: Router) => {
   router.get('/details', handelDefaultGetBlinkData);
   router.post('/action/buy/default/:amount', handelGetDefaultBuyTransaction);
-  // router.post('/action/sell/default/:amount', handelGetDefaultSellTransaction);
   router.get('/details/:id', handelGetBlinkMetaData);
-  router.get('/action/buy/:id/:amount', handelGetPaymentTransaction);
-  router.get('/action/sell/:id/:amount', handelGetBlinkMetaData);
+  router.post('/action/buy/:id/:amount', handelGetPaymentTransaction);
 });
