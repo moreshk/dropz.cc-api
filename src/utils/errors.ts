43f3aead531d2bb478aecf0e26fe67f123aed822
Pranav <pranavkp.me@outlook.com>
2024-06-26 12:10:30 +0530

    added console log

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -143,7 +143,8 @@ export function errorHandler(error: unknown, req: Request, res: Response<{
   const ip = req.ip;
   const url = req.originalUrl;
   const method = req.method;
-  // console.log(error);
+  // eslint-disable-next-line no-console
+  console.log(error);
   if (error instanceof BackendError) {
     message = error.message;
     code = error.code;
