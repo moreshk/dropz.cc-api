12994b99bddf56ac5f298827f715e2117e94e54b
Pranav <pranavkp.me@outlook.com>
2024-07-04 13:45:59 +0530

    added pagination

--- src/controllers/leaderboard-controllers.ts
+++ src/controllers/leaderboard-controllers.ts
@@ -1,13 +1,18 @@
+import { z } from 'zod';
 import { getLeaderBoard } from '@/services/leaderboard-services';
 import { getAllTokensList } from '@/services/token-services';
 import { createHandler } from '@/utils/create';
 import { BackendError } from '@/utils/errors';
 
-export const handelGetLeaderBoard = createHandler(async (req, res) => {
-  const leaderboard = await getLeaderBoard();
+export const handelGetLeaderBoard = createHandler(z.object({
+  query: z.object({
+    page: z.string().optional(),
+  }),
+}), async (req, res) => {
+  const page = req.query.page;
+  const leaderboard = await getLeaderBoard(page ? +page : 1);
   const tokens = await getAllTokensList();
-
   if (!leaderboard && !tokens)
     throw new BackendError('NOT_FOUND');
-  res.status(200).json({ leaderboard, tokens });
+  res.status(200).json({ ...leaderboard, tokens });
 });
--- src/services/leaderboard-services.ts
+++ src/services/leaderboard-services.ts
@@ -1,9 +1,12 @@
-import { desc } from 'drizzle-orm';
+import { count, desc } from 'drizzle-orm';
 import { leaderboard } from '@/schema/leaderboard';
 import { db } from '@/utils/db';
 
-export async function getLeaderBoard() {
-  return await db.query.leaderboard.findMany({
+const PAGE_SIZE = 10;
+
+export async function getLeaderBoard(page = 1) {
+  const offset = (page - 1) * PAGE_SIZE;
+  const leaderboardItems = await db.query.leaderboard.findMany({
     orderBy: [
       desc(leaderboard.points),
       desc(leaderboard.createdAt),
@@ -11,5 +14,13 @@ export async function getLeaderBoard() {
     with: {
       token: true,
     },
+    offset,
+    limit: PAGE_SIZE,
   });
+  const totalCount = await db.select({ count: count() }).from(leaderboard);
+
+  return {
+    leaderboard: leaderboardItems,
+    totalCount: totalCount[0]?.count,
+  };
 }
