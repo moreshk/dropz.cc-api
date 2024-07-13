4fa7c73f0d4a4bdeca13218d370e11039334451c
Pranav <pranavkp.me@outlook.com>
2024-07-13 10:39:50 +0530

    fix delete

--- src/routes/drop-routers.ts
+++ src/routes/drop-routers.ts
@@ -1,13 +1,12 @@
 import type { Router } from 'express';
 import { createRouter } from '@/utils/create';
-import { handleAddDrop, handleGetAllDrop } from '@/controllers/drop-controllers';
+import { handleAddDrop, handleDeleteDrop, handleGetAllDrop } from '@/controllers/drop-controllers';
 import { authenticate } from '@/middlewares/auth';
-import { handelDeleteToken } from '@/controllers/token-controllers';
 
 export default createRouter((router: Router) => {
   router.post('/add', authenticate({
     verifyAdmin: true,
   }), handleAddDrop);
   router.get('/all', handleGetAllDrop);
-  router.post('/delete', handelDeleteToken);
+  router.post('/delete', handleDeleteDrop);
 });
